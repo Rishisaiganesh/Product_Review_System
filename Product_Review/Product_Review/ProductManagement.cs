16c1fdfa296c071a5e7f28ce859942256093fae0
Rishisaiganesh <crazyganesh67@gmail.com>
2021-07-06 20:27:18 +0530

    Getting Top of Rating Products

---
+++ Product_Review/Product_Review/ProductManagement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Product_Review
+{
+    class ProductManagement
+    {
+        public readonly DataTable dataTable = new DataTable();
+        public void TopOfRecords(List<Review>Products)
+        {
+            var Records = (from Review in Products orderby Review.Rating descending select Review).Take(5);
+            foreach(var product in Records)
+            {
+                Console.WriteLine("ProductMangement");
+                Console.WriteLine("productsname" + product.ProductName + " " + "userID" + product.UserID + " " + " Rating" + product.Rating + "Review " + product.Product_Review);
+            }
+        }
+    }
+}
--- Product_Review/Product_Review/Program.cs
+++ Product_Review/Product_Review/Program.cs
@@ -20,12 +20,22 @@ namespace Product_Review
             new Review() { ProductName = "Mobails", UserID = 29, Rating = 6.5, Product_Review = "AVERAGE" },
             new Review() { ProductName = "Shirts", UserID = 30, Rating = 6, Product_Review = "AVERAGE" },
             new Review() { ProductName = "Sandles", UserID = 31, Rating = 5.5, Product_Review = "AVERAGE" },
-           
+             new Review() { ProductName = "PhotoFrame", UserID = 22, Rating = 10, Product_Review = "EXCELLENT" },
+            new Review() { ProductName= "Cars", UserID = 23, Rating = 9, Product_Review = "EXCELLENT" },
+            new Review() { ProductName = "WaterPurefier", UserID = 20, Rating = 8, Product_Review = "EXCELLENT" },
+            new Review() { ProductName = "Skechers", UserID = 19, Rating = 7.7, Product_Review = "GOOD" },
+            new Review() { ProductName = "Bottle", UserID = 16, Rating = 7.5, Product_Review = "GOOD" },
+            new Review() { ProductName = "BabyToys", UserID = 17, Rating = 7,Product_Review = "GOOD" },
+            new Review() { ProductName = "Healmets", UserID = 18, Rating = 6.8, Product_Review = "AVERAGE" },
             };
             foreach (var check in list)
             {
                 Console.WriteLine("ProductName" + check.ProductName + " " + "UserID" + check.UserID + " " + "Rating" + check.Rating + " " + "Product_Review" + check.Product_Review);
-                    }
+            }
+            {
+                ProductManagement mangement = new ProductManagement();
+                mangement.TopOfRecords(list);
+            }
 
         }
     }
