ec1b9a9e683e08006fbaf1993abf99cd923e1f70
Rishisaiganesh <crazyganesh67@gmail.com>
2021-07-06 21:02:22 +0530

    Retrive Product Review

--- Product_Review/Product_Review/ProductManagement.cs
+++ Product_Review/Product_Review/ProductManagement.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace Product_Review
 {
-    class ProductManagement
+   public class ProductManagement
     {
         public readonly DataTable dataTable = new DataTable();
         public void TopOfRecords(List<Review>Products)
@@ -16,6 +16,16 @@ namespace Product_Review
             {
                 Console.WriteLine("ProductMangement");
                 Console.WriteLine("productsname" + product.ProductName + " " + "userID" + product.UserID + " " + " Rating" + product.Rating + "Review " + product.Product_Review);
+            }
+        }
+     
+        public void Retrive(List<Review> Products)
+        {
+            var Records = (from Review in Products where Review.ProductName == "1" || Review.ProductName == "4" || Review.ProductName == "15" && Review.Rating > 3 select Review.Rating);
+            foreach (var products in Records)
+            {
+                Console.WriteLine("ProductNAme" + products.ProductName + " " + "ProductId" + products.UserID + "" + "ProductRating" + products.Rating + " " + "Product Review" + products.Product_Review);
+
             }
         }
     }
